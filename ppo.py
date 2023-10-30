b0a2d10100793da5da6fdba56daba3b62f795864
kasmello <karmel.gandahusada@gmail.com>
2023-10-30 19:21:43 +0800

    added callback for early stopping and evaluation

--- ppo.py
+++ ppo.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnNoModelImprovement
 from environment import CarParking
 
 # Making folder for end frames:
@@ -20,16 +21,25 @@ print(f"Created folder: {folder_path}")
 
 env = CarParking(60, 1_000)
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=1_000)
+stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=4, min_evals=100_000, verbose=1)
+eval_callback = EvalCallback(env, best_model_save_path="./logs/",
+                             log_path="./logs/", eval_freq=5_000,
+                             deterministic=True, render=False, verbose=1)
+
+model = PPO("MlpPolicy", env, verbose=1, learning_rate=0.001)
+model.learn(total_timesteps=2_000_000, progress_bar=True, callback=eval_callback)
 model.save("ppo_drive")
 
 
-model = PPO.load("ppo_drive")
 
-obs = env.reset()
-done = False
-while not done:
+model = PPO.load("ppo_drive", env=env)
+
+obs, _states = env.reset()
+terminated = False
+truncated = False
+while not (terminated or truncated):
+
     action, _states = model.predict(obs)
-    state, reward, done, info = env.step(action)
+
+    state, reward, terminated, truncated, info = env.step(action)
     env.render()
